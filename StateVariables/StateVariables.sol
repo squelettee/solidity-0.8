9fee70cf535cd170f1b636522ba33f0274453ee5
baptiste gresse <baptistegresse@MacBook-Pro-de-baptiste.local>
2024-03-13 00:29:21 +0100

    add StateVariables

---
+++ StateVariables/StateVariables.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract StateVariables {
+    uint256 public number = 345; //state variable
+    function foo() pure external {
+        uint256 num = 233; // local variable
+    }
+}
